60aad9b0a1560e0aff9d4b79845cf20b81b65278
theonlineaid <nkomorfaruk@gmail.com>
2024-03-10 16:57:59 +0600

    Add dockerfile to learn docker

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+const port = process.env.PORT || 9000;
+
+app.listen(9000, () => {
+    console.log('Server is running on port ' + port);
+})
